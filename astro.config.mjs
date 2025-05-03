7c6f0b69a9811e626592e6bc49f499ca84ff33f4
Adam <adamspeer@gmail.com>
2025-05-03 16:34:20 -0400

    debug build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: process.env.NODE_ENV === 'production' ? 'static' : 'server',
+  adapter:
+    process.env.NODE_ENV === 'production'
+      ? vercel({
+          analytics: true,
+        })
+      : node({
+          mode: 'standalone',
+        }),
   vite: {
     define: {
       'import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY': JSON.stringify(
